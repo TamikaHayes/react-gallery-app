ca713d48399cf6d76a731c5759a637c0ae858f77
TamikaHayes <tamika.hayes@gmail.com>
2021-05-21 18:56:48 -0700

    Updated PhotoContainer.js and App.js
    --successfully renders static placeholder images from Flickr to the browser

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 //import logo from './logo.svg';
 import React from 'react';
+import PhotoContainer from './PhotoContainer';
 
 const App = () => (
   <div className="container">
-
+    <PhotoContainer />
   </div>
 );
 
@@ -11,29 +12,3 @@ export default App;
 
 
 
-
-{/*import './App.css';
-
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        *<img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, world!
-        </p>
-        *<a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;*/}
