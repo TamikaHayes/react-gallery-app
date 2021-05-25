ed12f06ea87b51636ee04aa5aa18f304d339a1ea
TamikaHayes <tamika.hayes@gmail.com>
2021-05-25 16:14:10 -0700

    Updated PhotoContainer.js
    --removed conditional statement testing for presence of data
    
    --removed curly brace after data.map arrow function (which was causing 'photos' to return undefined)
    
    --uncommented {photos}  in return statement for "photo-container" <div>

--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -11,23 +11,27 @@ import Photo from './Photo';
 
 const PhotoContainer = (props) => {
     const {data} = props;
-    let photos;
-    
-    if (data.length > 0) {
-        photos = data.map(photo => {
+    let photos = data.map(photo => 
+        <Photo url={`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`} key={photo.id} />
+    )
+    {console.log('I am calling this from PhotoContainer')}
+    {console.log(photos)}
+
+    {/*if (data.length > 0) {
+    photos = data.map(photo => {
         
             <Photo url={`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`} key={photo.id} />
         });
     } else {
         console.log('Not Working');
-    }
+    }*/}
        
     return (
         <div className="photo-container">
             <h2>Results</h2>
             <ul>
-                {/*{photos}*/}
-                {console.log(photos)}
+                {photos}
+                {console.log(data.length)}
                 {console.log(props)}
             </ul>
         </div>
